ab7075325a445e31a14609475dd74e3a4e7a6569
fr0sty99 <j.neber@gmx.ch>
2022-02-08 17:11:54 +0100

    fix playground landing

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 require("dotenv").config();
 
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./typeDefs");
-const { resolvers } = require("./resolvers");
+import { ApolloServer } from "apollo-server";
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground
+} from "apollo-server-core";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     introspection: true,
-    playground: true,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
